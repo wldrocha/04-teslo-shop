37e71f71f80000aa5079abe7d7ce41e1bf84d96e
Wladimir Rocha <wld.rocha@gmail.com>
2024-02-08 13:17:30 -0400

    [ADD] fonts with next 13

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import { titleFont } from '@/config/font';
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="">
      <h1>Hello world</h1>
+     <h1 className={titleFont.className}>hello world</h1>
     </main>
   );
 }
---
+++ src/config/font.ts
@@ -0,0 +1,8 @@
+import { Inter, Montserrat } from 'next/font/google'
+
+export const inter = Inter({ subsets: ['latin'] })
+
+export const titleFont = Montserrat({
+  subsets: ['latin'],
+  weight: ['500', '700']
+})
