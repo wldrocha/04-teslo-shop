b50cf7e66570efd68e024fba439115b1f7c8a617
Wladimir Rocha <wld.rocha@gmail.com>
2024-02-09 16:39:08 -0400

    [REF] change data

--- src/components/products/product-grid/ProductGridItem.tsx
+++ src/components/products/product-grid/ProductGridItem.tsx
@@ -1,3 +1,6 @@
+'use client'
+import { useState } from 'react'
+
 import { Product } from '@/interfaces'
 import Image from 'next/image'
 import Link from 'next/link'
@@ -7,10 +10,20 @@ interface Props {
 }
 
 export const ProductGridItem = ({ product }: Props) => {
+  const [displayImage, setDisplayImage] = useState(product.images[0])
+
   return (
     <div className='rounded-md overflow-hidden fade-in'>
       <Link href={`/product/${product.slug}`}>
-        <Image src={`/products/${product.images[0]}`} alt={product.title} width={500} height={500} />
+        <Image
+          className='w-full object-cover rounded'
+          onMouseEnter={() => setDisplayImage(product.images[0])}
+          onMouseLeave={() => setDisplayImage(product.images[1])}
+          src={`/products/${displayImage}`}
+          alt={product.title}
+          width={500}
+          height={500}
+        />
       </Link>
       <div className='p-4 flex flex-col'>
         <Link className=' hover:text-blue-600' href={`/product/${product.slug}`}>
