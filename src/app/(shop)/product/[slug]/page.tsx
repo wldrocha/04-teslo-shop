9a1cc18eda4a95ca08d0c091610e4843079f5496
Wladimir Rocha <wld.rocha@gmail.com>
2024-02-15 14:28:48 -0400

    [ADD] product details

--- src/app/(shop)/product/[slug]/page.tsx
+++ src/app/(shop)/product/[slug]/page.tsx
@@ -1,7 +1,36 @@
-export default function ProductDetailPage () {
+import { titleFont } from '@/config/font'
+import { initialData } from '@/seed/seed'
+import { notFound } from 'next/navigation'
+
+interface Props {
+  params: {
+    slug: string
+  }
+}
+
+export default function ProductDetailPage({ params }: Props) {
+  const { slug } = params
+  const product = initialData.products.find((product) => product.slug === slug)
+  if (!product) {
+    notFound()
+  }
   return (
-    <div>
-      <h1>Product Details Page</h1>
+    <div className='mt-5 mb-20 grid grid-cols-1 md:grid-cols-3 gap-3'>
+      {/* slideshow */}
+      <div className='col-span-1 md:col-span-2'>gs</div>
+      {/* Product details */}
+      <div className='col-span-1 px-5'>
+        <h1 className={`${titleFont.className} antialiased font-bold text-xl`}>{product.title}</h1>
+
+        {/* size selector */}
+
+        {/* quantity selector */}
+        {/* button */}
+        <p className='text-lg mb-5'>{product.price}</p>
+        <button className='btn-primary my-5'>Add to Cart</button>
+        <h3 className='font-bold text-sm'>Description</h3>
+        <p className='font-light'>{product.description}</p>
+      </div>
     </div>
   )
 }
