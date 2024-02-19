2293d646466061df6686004ab73048a785c37f29
Wladimir Rocha <wld.rocha@gmail.com>
2024-02-19 09:54:08 -0400

    [ADD] login and register

---
+++ src/app/auth/new-account/page.tsx
@@ -0,0 +1,34 @@
+import { titleFont } from '@/config/font'
+import Link from 'next/link'
+
+export default function RegisterPage() {
+  return (
+    <div className='flex flex-col min-h-screen pt-32 sm:pt-52'>
+      <h1 className={`${titleFont.className} text-4xl mb-5`}>Ingresar</h1>
+
+      <div className='flex flex-col'>
+        <label htmlFor='email'>full name</label>
+        <input className='px-5 py-2 border bg-gray-200 rounded mb-5' type='text' />
+
+        <label htmlFor='email'>Email</label>
+        <input className='px-5 py-2 border bg-gray-200 rounded mb-5' type='email' />
+
+        <label htmlFor='email'>Pass</label>
+        <input className='px-5 py-2 border bg-gray-200 rounded mb-5' type='password' />
+
+        <button className='btn-primary'>Register</button>
+
+        {/* divisor l ine */}
+        <div className='flex items-center my-5'>
+          <div className='flex-1 border-t border-gray-500'></div>
+          <div className='px-2 text-gray-800'>O</div>
+          <div className='flex-1 border-t border-gray-500'></div>
+        </div>
+
+        <Link href='/auth/login' className='btn-secondary text-center'>
+          Login
+        </Link>
+      </div>
+    </div>
+  )
+}
