d025421ff3268e9254224987ff031a4ed1e04486
Wladimir Rocha <wld.rocha@gmail.com>
2024-02-26 00:52:07 -0400

    [ADD] redirect when products is equal to 0

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -1,5 +1,6 @@
 import { getPaginatedProductsWithImages } from '@/actions'
 import { ProductGrid, Title } from '@/components'
+import { redirect } from 'next/navigation'
 
 interface HomeProps {
   searchParams: {
@@ -10,6 +11,10 @@ interface HomeProps {
 export default async function Home({ searchParams }: HomeProps) {
   const page = searchParams.page ? parseInt(searchParams.page as string) : 1
   const { products } = await getPaginatedProductsWithImages({ page })
+
+  if (products.length === 0) {
+    redirect('/')
+  }
   return (
     <>
       <Title title='Shop' subtitle='All products' className='mb-2' />
