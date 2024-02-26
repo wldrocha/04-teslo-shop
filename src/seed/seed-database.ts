90bf4b680079988e753f0992adc4ae008db5568c
Wladimir Rocha <wld.rocha@gmail.com>
2024-02-25 23:51:05 -0400

    [ADD] seed categories

--- src/seed/seed-database.ts
+++ src/seed/seed-database.ts
@@ -1,9 +1,16 @@
 import { initialData } from './seed'
+import prisma from '../lib/prisma'
 
-async function main () {
-    // console.log(initialData)
-    console.log('Seeding database...')
+async function main() {
+  //Delete previous data
+  await Promise.all([prisma.productImage.deleteMany(), prisma.product.deleteMany(), prisma.category.deleteMany()])
+
+  const { categories, products } = initialData
+
+  const categoriesData = categories.map((category) => ({ name: category }))
+  await prisma.category.createMany({ data: categoriesData })
+  console.log('Seeding database...')
 }
-(()=>{
-    main()
-})
+;(() => {
+  main()
+})()
