dffa239475b5e5a22b3c376f2dd6f56d8582b7b7
Wladimir Rocha <wld.rocha@gmail.com>
2024-03-06 21:54:46 -0400

    [ADD] validations

--- src/app/(shop)/product/[slug]/ui/AddToCart.tsx
+++ src/app/(shop)/product/[slug]/ui/AddToCart.tsx
@@ -9,16 +9,27 @@ interface Props {
 
 export const AddToCart = ({ product }: Props) => {
   const [size, setSize] = useState<Size | undefined>()
+  const [quantity, setQuantity] = useState<number>(1)
+  const [posted, setPosted] = useState(false)
+
+  const AddToCart = () => {
+    setPosted(true)
+    if (!size) return
+  }
+
   return (
     <>
       {/* size selector */}
+      {posted && !size && <span className='mt-2 text-red-500 fade-in'>Please select size *</span>}
       <SizeSelector selectedSize={size} availableSizes={product.sizes} onSizeChanged={setSize} />
 
       {/* quantity selector */}
-      <QuantitySelector quantity={1} />
+      <QuantitySelector quantity={quantity} quantityChanged={setQuantity} />
       {/* button */}
       <p className='text-lg mb-5'>$ {product.price}</p>
-      <button className='btn-primary my-5'>Add to Cart</button>
+      <button className='btn-primary my-5' onClick={AddToCart}>
+        Add to Cart
+      </button>
     </>
   )
 }
