84970a46854e228f56ed7c478c1873c24850f5a4
Wladimir Rocha <wld.rocha@gmail.com>
2024-03-10 11:07:54 -0400

    [ADD] seed user

--- src/seed/seed-database.ts
+++ src/seed/seed-database.ts
@@ -6,8 +6,11 @@ async function main() {
   await prisma.productImage.deleteMany()
   await prisma.product.deleteMany()
   await prisma.category.deleteMany()
+  await prisma.user.deleteMany()
 
-  const { categories, products } = initialData
+  const { categories, products, users } = initialData
+
+  await prisma.user.createMany({ data: users })
 
   const categoriesData = categories.map((category) => ({ name: category }))
   await prisma.category.createMany({ data: categoriesData })
