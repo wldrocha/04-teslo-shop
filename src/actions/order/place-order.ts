f0992069b977da8cb6704ffc119ed7df081fa81f
Wladimir Rocha <wld.rocha@gmail.com>
2024-03-18 23:38:20 -0400

    [REF] update stock

--- src/actions/order/place-order.ts
+++ src/actions/order/place-order.ts
@@ -48,52 +48,103 @@ export const placeOrder = async (productsInOrder: ProductToOrder[], address: Add
 
   //create transaction
 
-  const prismaTx = await prisma.$transaction(async (tx) => {
-    // 1. update products stock
+  try {
+    const prismaTx = await prisma.$transaction(async (tx) => {
+      // 1. update products stock
 
-    // 2. create order - header - details
-    const order = await tx.order.create({
-      data: {
-        userId,
-        itemsInOrder,
-        subTotal,
-        tax,
-        total,
-        isPaid: false,
-        OrderItem: {
-          createMany: {
-            data: productsInOrder.map((p) => ({
-              quantity: p.quantity,
-              size: p.size,
-              productId: p.productId,
-              price: products.find((pr) => pr.id === p.productId)?.price ?? 0
-            }))
+      const updateProductsPromises = products.map(async (product) => {
+        // get the quantity of the product in the order
+        const productQuantity = productsInOrder
+          .filter((p) => p.productId === product.id)
+          .reduce((acc, p) => acc + p.quantity, 0)
+
+        if (productQuantity === 0) {
+          throw new Error(`Not enough stock for product ${product.id}`)
+        }
+        return tx.product.update({
+          where: { id: product.id },
+          data: {
+            // not should be
+            // inStock: product.inStock - productQuantity,
+
+            // recommended to use
+            inStock: {
+              decrement: productQuantity
+            }
+          }
+        })
+      })
+
+      const updateProducts = await Promise.all(updateProductsPromises)
+
+      // verify negative stock
+      updateProducts.forEach((product) => {
+        if (product.inStock < 0) {
+          throw new Error(`Negative stock for product ${product.id}`)
+        }
+      })
+
+      // 2. create order - header - details
+      const order = await tx.order.create({
+        data: {
+          userId,
+          itemsInOrder,
+          subTotal,
+          tax,
+          total,
+          isPaid: false,
+          OrderItem: {
+            createMany: {
+              data: productsInOrder.map((p) => ({
+                quantity: p.quantity,
+                size: p.size,
+                productId: p.productId,
+                price: products.find((pr) => pr.id === p.productId)?.price ?? 0
+              }))
+            }
           }
         }
+      })
+
+      // validation if price is 0
+      if (order.total === 0) {
+        throw new Error('Order total is 0')
+      }
+
+      // 3. create order address
+
+      const orderAddress = await tx.orderAddress.create({
+        data: {
+          orderId: order.id,
+          firstName: address.firstName,
+          lastName: address.lastName,
+          address: address.address,
+          address2: address.address2,
+          zip: address.zip,
+          city: address.city,
+          countryId: address.country,
+          phone: address.phone
+        }
+      })
+
+      return {
+        order,
+        orderAddress,
+        updatedProducts: updateProducts
       }
     })
-
-    // validation if price is 0
-    if (order.total === 0) {
-      throw new Error('Order total is 0')
-    }
-
-    // 3. create order address
-    const { country, ...restAddress } = address
-
-    const orderAddress = await tx.orderAddress.create({
-      data: {
-        orderId: order.id,
-        countryId: country,
-        ...restAddress
-      }
-    })
-
-    // 4.
 
     return {
-      order,
-      orderAddress
+      ok: true,
+      message: 'Order created successfully',
+      order: prismaTx.order,
+      prismaTx
     }
-  })
+  } catch (error: any) {
+    console.log(`🚀 ~ placeOrder ~ error:`, error)
+    return {
+      ok: false,
+      message: error.message
+    }
+  }
 }
